4d3f246ca92d7df1a19e847996364190825f2eda
Ramon Soares <ramon@codecraft63.com>
2017-02-05 09:19:51 -0300

    removed deprecated settings

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module Codecraft
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
 
     # Enable deflate / gzip compression of controller-generated responses
     config.middleware.use Rack::Deflater
