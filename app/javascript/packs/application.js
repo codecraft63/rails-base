47d7abe0c61d0809b1e606ac6bf781dacf66aabe
Ramon Soares <ramongsoares@gmail.com>
2019-08-30 19:10:11 -0300

    more a few line of code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,15 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+
+require("@rails/activestorage").start()
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
 //
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
 
 import Vue from 'vue/dist/vue.esm';
 import { sync } from 'vuex-router-sync';
